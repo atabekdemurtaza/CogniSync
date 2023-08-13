d47e0ae6102371cde71f9c2223d15cd60d3692a7
atabekdemurtaza <mychannel21war@gmail.com>
2023-08-14 01:31:59 +0500

    Token is

--- orbit/api/serializers.py
+++ orbit/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from orbit.models import Product, Category
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -18,13 +19,49 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
         fields = [
             'id',
             'username',
+            'name',
+            'isAdmin',
             'email',
             'first_name',
             'last_name',
-            'date_joined']
+            'date_joined'
+        ]
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == "":
+            name = obj.email
+        return name
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'name',
+            'isAdmin',
+            'email',
+            'first_name',
+            'last_name',
+            'date_joined',
+            'token',
+        ]
+
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token)
--- orbit/views/class_views.py
+++ orbit/views/class_views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from orbit.models import Product, Category
 from orbit.api.serializers import ProductSerializer, CategorySerializer
+from orbit.api.serializers import UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.viewsets import ModelViewSet
@@ -11,8 +12,9 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     def validate(self, attrs):
         data = super().validate(attrs)
-        data['username'] = self.user.username
-        data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
         return data
 
 
